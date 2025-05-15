824e8829e6520e484ac16f0ff5957bdf0570e2c3
chabeerabsal <chabeerabsal@gmail.com>
2025-05-15 22:23:39 +0530

    modified

--- src/IplAnalyser.java
+++ src/IplAnalyser.java
@@ -2,10 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class IplAnalyser {
     public static void main(String[] args) throws IOException {
@@ -15,6 +12,93 @@ public class IplAnalyser {
         matchesPlayedPerYear(matches);
         System.out.println("\n2. Matches won by each team:");
         matchesWonPerTeam(matches);
+        System.out.println("\n3 For the year 2016 get the extra runs conceded per team");
+        extraRunConcededPerTeam(matches,deliveries);
+
+        System.out.println("\n4 For the year 2015 get the top economical bowlers.");
+        topEconomicalbowler(matches,deliveries);
+    }
+
+    private static void topEconomicalbowler(List<String[]> matches, List<String[]> deliveries) {
+        HashSet<String> id=new HashSet<>();
+        for (String[] match : matches) {
+            if(match[1].equals("2015"))
+                id.add(match[0]);
+        }
+        Map<String,Integer> totalRuns=new HashMap<>();
+        for(String[]deliverie: deliveries) {
+            if(id.contains(deliverie[0])){
+                String bowler=deliverie[8];
+                int totalRun=Integer.parseInt(deliverie[17]);
+                totalRuns.put(bowler, totalRuns.getOrDefault(bowler, 0) + totalRun);
+            }
+        }
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(totalRuns.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+        System.out.println("Top Economical Bowlers: "+list.get(0).getKey());
+        Set<String> matchIds2015 = new HashSet<>();
+        for (String[] row : matches) {
+            if (row[1].equals("2015")) {
+                matchIds2015.add(row[0]);
+            }
+        }
+
+        Map<String, Integer> runs = new HashMap<>();
+        Map<String, Integer> balls = new HashMap<>();
+
+        for (String[] row : deliveries) {
+            if (matchIds2015.contains(row[0])) {
+                String bowler = row[8];
+                int run = Integer.parseInt(row[17]); // total_runs
+                runs.put(bowler, runs.getOrDefault(bowler, 0) + run);
+                balls.put(bowler, balls.getOrDefault(bowler, 0) + 1);
+            }
+        }
+
+        Map<String, Double> economy = new HashMap<>();
+        for (String bowler : runs.keySet()) {
+            if (balls.get(bowler) >= 60) {
+                double eco = runs.get(bowler) / (balls.get(bowler) / 6.0);
+                economy.put(bowler, eco);
+            }
+        }
+
+        economy.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .limit(5)
+                .forEach(e -> System.out.println(e.getKey() + ": " + String.format("%.2f", e.getValue())));
+
+          List<Map.Entry<String, Double>> list2 = new ArrayList<>(economy.entrySet());
+        list2.sort(Map.Entry.comparingByValue());
+        int count=0;
+        for(Map.Entry<String, Double> entry: list2) {
+            if(count==5)
+                break;
+            System.out.println(entry.getKey()+": "+entry.getValue());
+            count++;
+        }
+
+    }
+
+    private static void extraRunConcededPerTeam(List<String[]> matches, List<String[]> deliveries) {
+        HashSet<String> id=new HashSet<>();
+        for (String[] match : matches) {
+            if(match[1].equals("2016"))
+                id.add(match[0]);
+        }
+        Map<String,Integer> extraRuns=new HashMap<>();
+        for(String[]deliverie: deliveries) {
+            if(id.contains(deliverie[0])){
+                String bowlingTeam=deliverie[3];
+                int extraRun=Integer.parseInt(deliverie[16]);
+                extraRuns.put(bowlingTeam, extraRuns.getOrDefault(bowlingTeam, 0) + extraRun);
+            }
+        }
+        for(Map.Entry<String,Integer> entry: extraRuns.entrySet()) {
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }
+
+
     }
 
     private static void matchesWonPerTeam(List<String[]> matches) {
